b31eee24a0393ba33ae3cbb77315014e3a49a5de
Rasesh Rana <raseshrana@Raseshs-MacBook-Air.local>
2026-01-29 14:38:56 +0530

    g

--- backend/src/middlewares/connectDb.js
+++ backend/src/middlewares/connectDb.js
@@ -1,22 +1,8 @@
-// src/middlewares/connectDB.middleware.js
 import connectDB from "../db/index.js";
-
-let isConnected = false;
-let connectionError = null;
-
-async function ensureDb() {
-  if (connectionError) throw connectionError;
-  if (!isConnected) {
-    console.log("Attempting MongoDB connection...");
-    await connectDB();
-    isConnected = true;
-    console.log("✓ MongoDB connected");
-  }
-}
 
 export default async function connectDBMiddleware(req, res, next) {
   try {
-    await ensureDb();
+    await connectDB(); // uses cached connection
     next();
   } catch (err) {
     next(err);
